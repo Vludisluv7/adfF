18e0cc01f71ac8417aa1c419fcd899c11a6bebbb
Igor Chestnov <inchestnov@gmail.com>
2024-03-22 22:18:50 +0300

    fourth lesson

---
+++ src/main/java/ru/gb/lesson4/hw/Homework.java
@@ -0,0 +1,15 @@
+package ru.gb.lesson4.hw;
+
+public class Homework {
+
+  /**
+   * 1. Создать сущность Student с полями:
+   * 1.1 id - int
+   * 1.2 firstName - string
+   * 1.3 secondName - string
+   * 1.4 age - int
+   * 2. Подключить hibernate. Реализовать простые запросы: Find(by id), Persist, Merge, Remove
+   * 3. Попробовать написать запрос поиска всех студентов старше 20 лет (session.createQuery)
+   */
+
+}
